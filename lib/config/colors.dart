6aa7d7b66382ea6ac173af4af57292648f1c91c8
Sparsh-R17 <sparsh170702@gmail.com>
2023-01-13 14:34:02 +0530

    Custom Color Palette Added

---
+++ lib/config/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const green = Color(0xffC2D6BD);
+  static const blue = Color(0xffC9E3E3);
+  static const grey = Color(0xffF1F2F4);
+  static const brown = Color(0xffF2DAC6);
+  static const pink = Color(0xffF0E2E2);
+  static const yellow = Color(0xffF2EBC6);
+  static const lightBrown = Color(0xffFEEED5);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '/config/colors.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
@@ -22,6 +24,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
+List<Color> colorTesting = [
+  AppColor.blue,
+  AppColor.brown,
+  AppColor.green,
+  AppColor.grey,
+  AppColor.lightBrown,
+  AppColor.pink,
+  AppColor.yellow,
+];
+
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
@@ -31,8 +43,24 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('CFA'),
       ),
-      body: const Center(
-        child: Text('First Layout'),
+      body: ListView.builder(
+        itemCount: colorTesting.length,
+        itemBuilder: (ctx, i) {
+          return Center(
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              width: 200,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: colorTesting[i],
+              ),
+              child: const Center(
+                child: Text('Testing'),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
