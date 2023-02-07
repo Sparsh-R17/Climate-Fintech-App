85321f70f9c3b1fce9cef1fa84ae81bd3ae91175
bishnoitejal21 <tejalbishnoi21@gmail.com>
2023-02-08 02:25:14 +0530

    SignUp and Login added

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,7 @@
+import 'package:climate_fintech_app/screens/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   static const routeName = '/login';
@@ -15,13 +17,136 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: const Color(0xfffafafa),
       body: Stack(
-        children: [
+        children: <Widget>[
           SizedBox(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             child: SvgPicture.asset(
               'assets/images/svg/login_pattern.svg',
               fit: BoxFit.cover,
+            ),
+          ),
+          // Container(
+          //   alignment: Alignment.topLeft,
+          //   padding: EdgeInsets.only(
+          //     top: MediaQuery.of(context).size.height * 0.25,
+          //     left: MediaQuery.of(context).size.width * 0.1,
+          //     right: MediaQuery.of(context).size.width * 0.2,
+          //   ),
+          //   child: Text(
+          //     'Welcome\nBack',
+          //     style: GoogleFonts.poppins(
+          //       color: Colors.black,
+          //       fontWeight: FontWeight.bold,
+          //       fontSize: 25,
+          //     ),
+          //   ),
+          // ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.1,
+                left: MediaQuery.of(context).size.width * 0.05,
+                right: MediaQuery.of(context).size.width * 0.05,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.only(
+                      //top: MediaQuery.of(context).size.height * 0.25,
+                      //left: MediaQuery.of(context).size.width * 0.1,
+                      right: MediaQuery.of(context).size.width * 0.2,
+                    ),
+                    child: Text(
+                      'Welcome\nBack',
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.025,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Sign In',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: MediaQuery.of(context).size.height * 0.04,
+                        backgroundColor: Colors.black,
+                        child: IconButton(
+                          onPressed: (() {
+                            print('Signed In');
+                          }),
+                          icon: const Icon(Icons.arrow_forward),
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      TextButton(
+                        onPressed: (() {
+                          print('SignUp');
+                          Navigator.pushNamed(context, SignUp.routeName);
+                        }),
+                        child: Text(
+                          'Sign Up',
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            decoration: TextDecoration.underline,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                          onPressed: (() {
+                            print(
+                              'Forget Password',
+                            );
+                          }),
+                          child: Text(
+                            'Forget Password',
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              decoration: TextDecoration.underline,
+                              fontSize: 15,
+                            ),
+                          ))
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ],
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignUp extends StatefulWidget {
   static const routeName = '/sign-up';
@@ -25,10 +26,95 @@ class _SignUpState extends State<SignUp> {
                 fit: BoxFit.cover,
               ),
             ),
-            //Add your design code here
-            const Center(
-              child: Text('Welcome Back'),
-            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.07,
+                  right: MediaQuery.of(context).size.width * 0.07,
+                ),
+                child: Column(children: [
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.18,
+                      right: MediaQuery.of(context).size.width * 0.2,
+                    ),
+                    child: Text(
+                      'Create\nAccount',
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Name',
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Sign Up',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: MediaQuery.of(context).size.height * 0.04,
+                        backgroundColor: Colors.black,
+                        child: IconButton(
+                          onPressed: (() {
+                            print('Signed Up');
+                          }),
+                          icon: const Icon(Icons.arrow_forward),
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                  ),
+                  TextButton(
+                    onPressed: (() {
+                      print('Already have an account');
+                    }),
+                    child: Text(
+                      'Already Have an Account?',
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            )
           ],
         ),
       ),
